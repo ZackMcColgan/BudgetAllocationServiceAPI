a8c6bd4a96d6ffb5832eef7c4eea0f9b5358e259
Zack McColgan <zackmccolg@gmail.com>
2020-10-30 18:46:16 -0500

    Setting up the budget allocation service

---
+++ BudgetAllocationsService/BudgetAllocationsService/src/Services/DefaultBudgetAllocationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BudgetAllocationsService.Models;
+
+namespace BudgetAllocationsService.Services
+{
+    public sealed class DefaultBudgetAllocationService : IBudgetAllocationService
+    {
+        private readonly BudgetAllocationContext _context;
+
+        public DefaultBudgetAllocationService(BudgetAllocationContext context)
+        {
+            _context = context;
+        }
+
+        // budgetAllocationsView
+        /*
+        public async Task<CommentResource> GetBudgetAllocationViewAsync(Guid id, CancellationToken ct)
+        {
+            var entity = await _context
+                .Comments
+                .SingleOrDefaultAsync(x => x.Id == id, ct);
+
+            return Mapper.Map<CommentResource>(entity);
+        }
+        */
+    }
+}
